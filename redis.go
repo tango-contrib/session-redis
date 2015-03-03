ae140fb2e5c6100c2989e4e10a3413c6fef58f21
Lunny Xiao <xiaolunwen@gmail.com>
2015-03-03 17:22:36 +0800

    init

---
+++ redis.go
@@ -0,0 +1,262 @@
+// Copyright 2013 Beego Authors
+// Copyright 2014 Unknwon
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+package session
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"reflect"
+	"time"
+	"unsafe"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/lunny/log"
+	"github.com/lunny/tango"
+
+	"github.com/tango-contrib/session"
+)
+
+var _ session.Store = &RedisStore{}
+
+type Options struct {
+	Host     string
+	Port     string
+	Password string
+	DbIndex  int
+	MaxAge   time.Duration
+}
+
+// RedisStore represents a redis session store implementation.
+type RedisStore struct {
+	Options
+	Logger tango.Logger
+	pool   *redis.Pool
+}
+
+func preOptions(opts []Options) Options {
+	var opt Options
+	if len(opts) > 0 {
+		opt = opts[0]
+	}
+	if opt.Host == "" {
+		opt.Host = "127.0.0.1"
+	}
+	if opt.Port == "" {
+		opt.Port = "6379"
+	}
+	return opt
+}
+
+// NewRedisStore creates and returns a redis session store.
+func NewRedisStore(opts ...Options) *RedisStore {
+	opt := preOptions(opts)
+	var pool = &redis.Pool{
+		MaxIdle:     5,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			// the redis protocol should probably be made sett-able
+			c, err := redis.Dial("tcp", opt.Host+":"+opt.Port)
+			if err != nil {
+				return nil, err
+			}
+			if len(opt.Password) > 0 {
+				if _, err := c.Do("AUTH", opt.Password); err != nil {
+					c.Close()
+					return nil, err
+				}
+			} else {
+				// check with PING
+				if _, err := c.Do("PING"); err != nil {
+					c.Close()
+					return nil, err
+				}
+			}
+			return c, err
+		},
+		// custom connection test method
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if _, err := c.Do("PING"); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+
+	return &RedisStore{
+		Options: opt,
+		pool:    pool,
+		Logger:  log.Std,
+	}
+}
+
+/*
+func (c *RedisStore) getObject(key string) interface{} {
+	conn := c.pool.Get()
+	defer conn.Close()
+	raw, err := conn.Do("GET", key)
+	if raw == nil {
+		return nil
+	}
+	item, err := redis.Bytes(raw, err)
+	if err != nil {
+		c.Logger.Errorf("redis.Bytes failed: %s", err)
+		return nil
+	}
+
+	value, err := c.deserialize(item)
+	if err != nil {
+		return nil
+	}
+	return value
+}*/
+
+func (c *RedisStore) serialize(value interface{}) ([]byte, error) {
+	err := c.registerGobConcreteType(value)
+	if err != nil {
+		return nil, err
+	}
+
+	if reflect.TypeOf(value).Kind() == reflect.Struct {
+		return nil, fmt.Errorf("serialize func only take pointer of a struct")
+	}
+
+	var b bytes.Buffer
+	encoder := gob.NewEncoder(&b)
+
+	err = encoder.Encode(&value)
+	if err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
+}
+
+func (c *RedisStore) deserialize(byt []byte) (ptr interface{}, err error) {
+	b := bytes.NewBuffer(byt)
+	decoder := gob.NewDecoder(b)
+
+	var p interface{}
+	err = decoder.Decode(&p)
+	if err != nil {
+		return
+	}
+
+	v := reflect.ValueOf(p)
+	if v.Kind() == reflect.Struct {
+		var pp interface{} = &p
+		datas := reflect.ValueOf(pp).Elem().InterfaceData()
+
+		sp := reflect.NewAt(v.Type(),
+			unsafe.Pointer(datas[1])).Interface()
+		ptr = sp
+	} else {
+		ptr = p
+	}
+	return
+}
+
+func (c *RedisStore) registerGobConcreteType(value interface{}) error {
+	t := reflect.TypeOf(value)
+
+	switch t.Kind() {
+	case reflect.Ptr:
+		v := reflect.ValueOf(value)
+		i := v.Elem().Interface()
+		gob.Register(i)
+	case reflect.Struct, reflect.Map, reflect.Slice:
+		gob.Register(value)
+	case reflect.String, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Bool, reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128:
+		// do nothing since already registered known type
+	default:
+		return fmt.Errorf("unhandled type: %v", t)
+	}
+	return nil
+}
+
+// Set sets value to given key in session.
+func (s *RedisStore) Set(id session.Id, key string, val interface{}) error {
+	bs, err := s.serialize(val)
+	if err != nil {
+		return err
+	}
+	_, err = s.Do("HSET", id, key, bs)
+	return err
+}
+
+// Get gets value by given key in session.
+func (s *RedisStore) Get(id session.Id, key string) interface{} {
+	val, err := s.Do("HGET", id, key)
+	if err != nil {
+		s.Logger.Errorf("redis HGET failed: %s", err)
+		return nil
+	}
+
+	item, err := redis.Bytes(val, err)
+	if err != nil {
+		s.Logger.Errorf("redis.Bytes failed: %s", err)
+		return nil
+	}
+
+	value, err := s.deserialize(item)
+	if err != nil {
+		s.Logger.Errorf("redis HGET failed: %s", err)
+		return nil
+	}
+	return value
+}
+
+// Delete delete a key from session.
+func (s *RedisStore) Del(id session.Id, key string) bool {
+	_, err := s.Do("HDEL", id, key)
+	return err == nil
+}
+
+func (s *RedisStore) Clear(id session.Id) bool {
+	_, err := s.Do("DEL", id)
+	return err == nil
+}
+
+func (s *RedisStore) Add(id session.Id) bool {
+	return false
+}
+
+func (s *RedisStore) Do(cmd string, args ...interface{}) (interface{}, error) {
+	conn := s.pool.Get()
+	defer conn.Close()
+	return conn.Do(cmd, args...)
+}
+
+func (s *RedisStore) Exist(id session.Id) bool {
+	has, err := s.Do("EXISTS", id)
+	return err == nil && has.(bool)
+}
+
+func (s *RedisStore) SetMaxAge(maxAge time.Duration) {
+	s.MaxAge = maxAge
+}
+
+func (s *RedisStore) SetIdMaxAge(id session.Id, maxAge time.Duration) {
+
+}
+
+func (s *RedisStore) Ping() error {
+	_, err := s.Do("Ping")
+	return err
+}
+
+func (s *RedisStore) Run() error {
+	return nil
+}
---
+++ redis_test.go
@@ -0,0 +1,101 @@
+// Copyright 2015 tango authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+package session
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/lunny/tango"
+	"github.com/tango-contrib/session"
+)
+
+type SessionAction struct {
+	session.Session
+}
+
+type Test struct {
+	Id   int64
+	Name string
+}
+
+func (action *SessionAction) Get() string {
+	var test1 = []string{"1", "2"}
+	action.Session.Set("11", test1)
+	res := action.Session.Get("11")
+
+	fmt.Println(res.([]string))
+
+	var test2 = map[string]int{"1": 1, "2": 2}
+	action.Session.Set("22", test2)
+	res2 := action.Session.Get("22")
+
+	fmt.Println(res2.(map[string]int))
+
+	for i := 0; i < 3; i++ {
+		var test3 = Test{1, "xlw"}
+		action.Session.Set("33", &test3)
+		res3 := action.Session.Get("33")
+
+		fmt.Println(res3.(*Test))
+	}
+
+	action.Session.Set("test", "1")
+	return action.Session.Get("test").(string)
+}
+
+func TestSession(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	recorder := httptest.NewRecorder()
+	recorder.Body = buff
+
+	tg := tango.Classic()
+	tg.Use(session.New(session.Options{
+		Store: NewRedisStore(Options{
+			Host:    "127.0.0.1",
+			DbIndex: 0,
+			MaxAge:  30 * time.Minute,
+		}),
+	}))
+	tg.Get("/", new(SessionAction))
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), "1")
+}
+
+/* Test Helpers */
+func expect(t *testing.T, a interface{}, b interface{}) {
+	if a != b {
+		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+	}
+}
+
+func refute(t *testing.T, a interface{}, b interface{}) {
+	if a == b {
+		t.Errorf("Did not expect %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+	}
+}
